4585e6da9da652d021b5ff4b10ef41fa77cd00ed
leis33 <l.abdulah@abv.bg>
2026-01-04 17:44:33 +0200

    feat: add Session Factory

---
+++ src/main/java/nbu/dbProject/configuration/SessionFactoryUtil.java
@@ -0,0 +1,29 @@
+package nbu.dbProject.configuration;
+
+import nbu.dbProject.entity.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class SessionFactoryUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.addAnnotatedClass(Company.class);
+            configuration.addAnnotatedClass(Employee.class);
+            configuration.addAnnotatedClass(Apartment.class);
+            configuration.addAnnotatedClass(Building.class);
+            configuration.addAnnotatedClass(Pet.class);
+            configuration.addAnnotatedClass(Resident.class);
+            configuration.addAnnotatedClass(Payment.class);
+            ServiceRegistry serviceRegistry
+                    = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
+    }
+}
