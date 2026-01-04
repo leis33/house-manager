7888e6f456242de3d788ede8d4a2e1f14b14d520
leis33 <l.abdulah@abv.bg>
2026-01-04 18:06:20 +0200

    refactor: add annotations to Company

--- src/main/java/nbu/dbProject/entity/Company.java
+++ src/main/java/nbu/dbProject/entity/Company.java
@@ -2,11 +2,28 @@ package nbu.dbProject.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import nbu.dbProject.validator.InvalidNames;
+
 import java.util.List;
 
 @Entity
+@Table(name = "company")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Builder
 public class Company extends BaseEntity {
-    @NotBlank
+
+    @Column(name = "name")
+    @InvalidNames(message = "The name is not valid!")
+    @NotBlank(message = "Company name cannot be blank!")
+    @Size(max = 20, message = "Company name has to be with up to 20 characters!")
+    @Pattern(regexp = "^([A-Z]).*", message = "Company name has to start with capital letter!")
     private String name;
 
     private double revenue;
